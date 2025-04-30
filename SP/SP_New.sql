0eb585f9abfd82bdb8b2cf4761d85adf9b7ba805
KAPIL <kapil.khot9530@gmail.com>
2025-04-30 17:14:44 +0530

    Adding new file and modified one SP

---
+++ SP/SP_New.sql
@@ -0,0 +1,17 @@
+USE [KPSM_GIT]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_classDetails]    Script Date: 4/28/2025 11:54:31 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[SP_New]
+as
+BEGIN
+ 
+ SELECT * FROM dimsource
+
+
+
+END
+GO
